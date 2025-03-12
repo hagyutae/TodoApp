d00c74bfbd89b01b861e756967ca567f14856b58
sangyeobhan <sangyeobhandev@gmail.com>
2025-03-12 16:41:28 +0900

    refact: 생성자 lombok으로 처리

--- src/main/java/com/sb02/todoapp/controller/TodoRestController.java
+++ src/main/java/com/sb02/todoapp/controller/TodoRestController.java
@@ -4,19 +4,17 @@ import com.sb02.todoapp.dto.TodoRequestDto;
 import com.sb02.todoapp.dto.TodoResponseDto;
 import com.sb02.todoapp.dto.TodosResponseDto;
 import com.sb02.todoapp.service.TodoService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/todos")
+@RequiredArgsConstructor
 public class TodoRestController {
 
     private final TodoService todoService;
-
-    public TodoRestController(TodoService todoService) {
-        this.todoService = todoService;
-    }
 
     @GetMapping()
     public TodosResponseDto getAllTodos() {
--- src/main/java/com/sb02/todoapp/service/TodoService.java
+++ src/main/java/com/sb02/todoapp/service/TodoService.java
@@ -4,18 +4,16 @@ import com.sb02.todoapp.dto.TodoRequestDto;
 import com.sb02.todoapp.dto.TodoResponseDto;
 import com.sb02.todoapp.entity.Todo;
 import com.sb02.todoapp.repository.TodoRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class TodoService {
 
     private final TodoRepository todoRepository;
-
-    public TodoService(TodoRepository todoRepository) {
-        this.todoRepository = todoRepository;
-    }
 
     public List<TodoResponseDto> getAllTodos() {
         return todoRepository.findAll().stream()
