f9df5c47a65fc6ef95cafc7a491e06a0f5655b64
JaeKwan AN <jaekwan99@gmail.com>
2025-03-12 22:22:53 +0900

    Feat: Todo 목록 조회 API 반환 형식 추가

--- src/main/java/com/sb02/todoapp/controller/TodoRestController.java
+++ src/main/java/com/sb02/todoapp/controller/TodoRestController.java
@@ -1,4 +1,31 @@
 package com.sb02.todoapp.controller;
 
+import com.sb02.todoapp.model.Todo;
+import com.sb02.todoapp.service.TodoService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/todos")
 public class TodoRestController {
+    private final TodoService todoService;
+
+    public TodoRestController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Map<String, String>> addTodo(@RequestBody Todo todo) {
+        todoService.addTodo(todo);
+        return ResponseEntity.ok(Map.of("message", "성공"));
+    }
+
+    @GetMapping
+    public ResponseEntity<Map<String, List<Todo>>> getTodos() {
+        List<Todo> todos = todoService.getTodos();
+        return ResponseEntity.ok(Map.of("todos", todos));
+    }
 }
