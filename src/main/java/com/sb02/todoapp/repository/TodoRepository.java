af0ff89c580cf639351c615cf44f481d1cd6c72b
홍길동 <your-email@example.com>
2025-03-12 16:12:22 +0900

    todo간단한 구현

--- src/main/java/com/sb02/todoapp/repository/TodoRepository.java
+++ src/main/java/com/sb02/todoapp/repository/TodoRepository.java
@@ -1,4 +1,23 @@
 package com.sb02.todoapp.repository;
 
+import com.sb02.todoapp.Todo;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+@Repository
 public class TodoRepository {
+    private final Map<UUID, Todo> todos = new HashMap<>();
+    public ArrayList<Todo> findAll() {
+        return new ArrayList<>(todos.values());
+    }
+    public Todo save(Todo todo) {
+        if (todo.getId() == null) {
+            UUID uuid=UUID.randomUUID();
+            todos.put(uuid, todo);
+        } else {
+            todos.put(todo.getId(), todo);
+        }
+        return todo;
+    }
+
 }
