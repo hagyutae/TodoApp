5837959406f23bf5792aa33500dd811b7a499bbd
Bokyung Kim <chloeee0228@gmail.com>
2025-03-12 16:19:00 +0900

    feat: todoapp

--- src/main/java/com/sb02/todoapp/controller/TodoRestController.java
+++ src/main/java/com/sb02/todoapp/controller/TodoRestController.java
@@ -1,4 +1,30 @@
 package com.sb02.todoapp.controller;
 
+import com.sb02.todoapp.Todo;
+import com.sb02.todoapp.service.TodoService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todos")
 public class TodoRestController {
+
+    private final TodoService todoService;
+
+    public TodoRestController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping
+    public List<Todo> getAllTodos(){
+        return todoService.getAllTodos();
+    }
+
+    @PostMapping
+    public Todo addTodo(@RequestBody Todo todo){
+        return todoService.addTodo(todo);
+
+    }
 }
--- src/main/java/com/sb02/todoapp/repository/TodoRepository.java
+++ src/main/java/com/sb02/todoapp/repository/TodoRepository.java
@@ -1,4 +1,27 @@
 package com.sb02.todoapp.repository;
 
+import com.sb02.todoapp.Todo;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
 public class TodoRepository {
+
+    private static Map<Long, Todo> todos = new HashMap<>();
+    private static Long idCounter = 0L;
+
+    public List<Todo> findAll() {
+        return new ArrayList<>(todos.values());
+    }
+
+    public Todo save(Todo todo) {
+        todo.setId(idCounter);
+        todos.put(idCounter++, todo);
+        return todo;
+    }
+
 }
