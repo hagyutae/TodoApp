75f7d8803224442db9cb700b3fddc1f0cc3175fd
hanna kong <khn1346@gmail.com>
2025-03-12 16:44:20 +0900

    feat: Todo entity 구현

---
+++ src/main/java/com/sb02/todoapp/entity/Todo.java
@@ -0,0 +1,30 @@
+package com.sb02.todoapp.entity;
+
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+public class Todo {
+    private UUID id;
+    private String name;
+    private String description;
+    private LocalDateTime createdAt;
+
+    public Todo(String name, String description) {
+        this.id = UUID.randomUUID();
+        this.name = name;
+        this.description = description;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @Override
+    public String toString() {
+        return "TodoEntity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
