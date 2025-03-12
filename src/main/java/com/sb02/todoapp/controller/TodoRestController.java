3387aa37262c7d9f67370305dfc41727ac7e8044
ddday366 <ddday366@gmail.com>
2025-03-12 17:06:33 +0900

    feat: TodoRestController 작성

--- src/main/java/com/sb02/todoapp/controller/TodoRestController.java
+++ src/main/java/com/sb02/todoapp/controller/TodoRestController.java
@@ -1,4 +1,29 @@
 package com.sb02.todoapp.controller;
 
+import com.sb02.todoapp.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/todos")
 public class TodoRestController {
+    private final TodoService todoService;
+
+    @Autowired
+    public TodoRestController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping
+    public List<Map<String, String>> getTodos() {
+        return todoService.getTodos();
+    }
+
+    @PostMapping
+    public Map<String, String> creatTodo(@RequestBody Map<String, String> todoData) {
+        return todoService.creatTodo(todoData);
+    }
 }
