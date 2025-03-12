e15b70c91f1fece8544646fc96e7efbdab490d5b
정종현 <bellhyunforgithub@gmail.com>
2025-03-12 17:38:33 +0900

    Feat: api 구현, html 일부 수정

--- src/main/java/com/sb02/todoapp/controller/TodoRestController.java
+++ src/main/java/com/sb02/todoapp/controller/TodoRestController.java
@@ -1,4 +1,29 @@
 package com.sb02.todoapp.controller;
 
+import com.sb02.todoapp.entity.Todo;
+import com.sb02.todoapp.service.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todos")
 public class TodoRestController {
+    private final TodoService todoService;
+
+    public TodoRestController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @PostMapping
+    public Todo save(@RequestBody Todo todo) {
+        todoService.save(todo);
+        return todo;
+    }
+
+    @GetMapping
+    public List<Todo> getTodos(){
+        return todoService.getTodos();
+    }
+
 }
--- src/main/java/com/sb02/todoapp/repository/TodoRepository.java
+++ src/main/java/com/sb02/todoapp/repository/TodoRepository.java
@@ -1,4 +1,25 @@
 package com.sb02.todoapp.repository;
 
+import com.sb02.todoapp.entity.Todo;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
 public class TodoRepository {
+    private final List<Todo> data;
+
+    public TodoRepository() {
+        this.data = new ArrayList<>();
+    }
+
+    public Todo save(Todo todo){
+        data.add(todo);
+        return todo;
+    }
+
+    public List<Todo> getTodos(){
+        return this.data;
+    }
 }
