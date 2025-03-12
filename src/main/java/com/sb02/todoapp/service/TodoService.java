5b1083cf3ffe68c5246bccc387d5ab288b5a0b98
hyorim <tg305@naver.com>
2025-03-12 16:51:13 +0900

    feat : todo 추가, 조회

--- src/main/java/com/sb02/todoapp/service/TodoService.java
+++ src/main/java/com/sb02/todoapp/service/TodoService.java
@@ -1,4 +1,22 @@
 package com.sb02.todoapp.service;
 
+import com.sb02.todoapp.Todo;
+import com.sb02.todoapp.repository.TodoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TodoService {
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository){
+        this.todoRepository = todoRepository;
+    }
+    public List<Todo> getTodos(){
+        return todoRepository.findAll();
+    }
+    public void addTodo(String name,String description){
+        todoRepository.save(new Todo(name,description));
+    }
 }
