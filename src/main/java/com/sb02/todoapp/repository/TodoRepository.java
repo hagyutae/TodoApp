ca2d9f4fa4b26082fea44940953792d7163947d0
ddday366 <ddday366@gmail.com>
2025-03-12 17:06:45 +0900

    feat: TodoRepository 작성

--- src/main/java/com/sb02/todoapp/repository/TodoRepository.java
+++ src/main/java/com/sb02/todoapp/repository/TodoRepository.java
@@ -1,4 +1,20 @@
 package com.sb02.todoapp.repository;
 
+import com.sb02.todoapp.Todo;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
 public class TodoRepository {
+    private final List<Todo> todos = new ArrayList<>();
+
+    public List<Todo> findAll() {
+        return todos;
+    }
+
+    public void save(Todo todo) {
+        todos.add(todo);
+    }
 }
