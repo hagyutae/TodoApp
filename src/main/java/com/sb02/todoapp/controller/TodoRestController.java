17e974728690ec033bfe605a6a10cdf36a274085
김민식 <u1r2r3@naver.com>
2025-03-12 19:00:10 +0900

    feat: Spring을 이용하여 Todo 앱 기능 구현

--- src/main/java/com/sb02/todoapp/controller/TodoRestController.java
+++ src/main/java/com/sb02/todoapp/controller/TodoRestController.java
@@ -1,4 +1,29 @@
 package com.sb02.todoapp.controller;
 
+import com.sb02.todoapp.entity.Todo;
+import com.sb02.todoapp.service.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todos")
 public class TodoRestController {
+    private final TodoService todoService;
+
+    public TodoRestController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping
+    public List<Todo> getTodos() {
+        // todo 정보가 있는 List 반환
+        return todoService.getAllTodos();
+    }
+
+    @PostMapping
+    public List<Todo> createTodo(@RequestBody Todo todo) {
+        // todo 정보 추가
+        return todoService.createTodo(todo);
+    }
 }
