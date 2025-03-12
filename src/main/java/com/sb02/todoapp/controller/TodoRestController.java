8050181b95da4e632f2d733fa94d3b009cc64ec8
WaiCat <98246479+WaiCat@users.noreply.github.com>
2025-03-12 16:18:33 +0900

    Todo 구현

--- src/main/java/com/sb02/todoapp/controller/TodoRestController.java
+++ src/main/java/com/sb02/todoapp/controller/TodoRestController.java
@@ -1,4 +1,32 @@
 package com.sb02.todoapp.controller;
 
+
+import com.sb02.todoapp.Entity.Todo;
+import com.sb02.todoapp.service.TodoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/todos")
 public class TodoRestController {
+
+    private final TodoService todoService;
+
+    public TodoRestController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @GetMapping
+    public List<Todo> getTodos(){
+        List<Todo> todos = todoService.getAll();
+        System.out.println("서버에서 반환하는 TODO 목록: " + todos); // 📌 서버 로그 확인
+        return todoService.getAll();
+    }
+
+    @PostMapping
+    public Todo addTodos(@RequestBody Todo todo){
+        todoService.addTodo(todo);
+        return todo;
+    }
 }
--- src/main/java/com/sb02/todoapp/repository/TodoRepository.java
+++ src/main/java/com/sb02/todoapp/repository/TodoRepository.java
@@ -1,4 +1,21 @@
 package com.sb02.todoapp.repository;
 
+import com.sb02.todoapp.Entity.Todo;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
 public class TodoRepository {
+    public final List<Todo> todoList = new ArrayList<>();
+
+    public List<Todo> findAll(){
+        return todoList;
+    }
+
+    public void addTodo(Todo todo){
+        todoList.add(todo);
+    }
+
 }
